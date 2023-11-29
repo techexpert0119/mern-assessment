889d36c3995fb1fddb3e774bb9541a31ee8ad2db
TechExpert0119 <techexpert0119@gmail.com>
2023-11-29 09:51:15 +0000

    FE-Rename page components

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,5 @@
-export { default as HomePage } from "./HomePage";
-export { default as LoginPage } from "./LoginPage";
-export { default as RegisterPage } from "./RegisterPage";
-export { default as ForgotPasswordPage } from "./ForgotPasswordPage";
-export { default as PasswordResetPage } from "./PasswordResetPage";
+export { default as HomePage } from "./Home";
+export { default as LoginPage } from "./Login";
+export { default as RegisterPage } from "./Register";
+export { default as ForgotPasswordPage } from "./ForgotPassword";
+export { default as PasswordResetPage } from "./PasswordReset";
